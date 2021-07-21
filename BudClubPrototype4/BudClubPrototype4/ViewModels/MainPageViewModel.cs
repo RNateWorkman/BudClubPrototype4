827faf5f0a20fc3d7e381e94830373dc136d65e8
chardad428 <rnnnatee@gmail.com>
2021-07-21 11:56:04 -0400

    Add project files.

---
+++ BudClubPrototype4/BudClubPrototype4/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BudClubPrototype4
+{
+    class MainPageViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public MainPageViewModel()
+        {
+            OpenQRReaderCommand = new Command(async () =>
+            {
+                var qrVM = new ViewModels.QRReaderPageViewModel();
+
+                var qrPage = new Views.QRReaderPage();
+
+                qrPage.BindingContext = qrVM;
+
+                await Application.Current.MainPage.Navigation.PushAsync(qrPage);
+            });
+
+            OpenPhoneNumberPageCommand = new Command(async () =>
+            {
+                var phoneVM = new ViewModels.PhoneNumberPageViewModel();
+
+                var phonePage = new Views.PhoneNumberPage();
+
+                await Application.Current.MainPage.Navigation.PushAsync(phonePage);
+            });
+
+        }
+             public Command OpenQRReaderCommand { get; }
+             public Command OpenPhoneNumberPageCommand { get; }
+
+    }
+
+}
+
+
+
---
+++ BudClubPrototype4/BudClubPrototype4/ViewModels/PhoneNumberPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudClubPrototype4.ViewModels
+{
+    class PhoneNumberPageViewModel
+    {
+    }
+}
